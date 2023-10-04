5ca2aafc5c03bdf2b6606e3ddb4f8207757a62fd
mirjalol <mirislomovmirjalol@gmail.com>
2023-10-04 15:15:42 +0100

    set up project

---
+++ src/main/java/com/JJEP/JJEP/JjepApplication.java
@@ -0,0 +1,13 @@
+package com.JJEP.JJEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjepApplication.class, args);
+	}
+
+}
