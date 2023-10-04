22410d4c2076ad01ff010b5dac0025a94c945ea8
mirjalol <mirislomovmirjalol@gmail.com>
2023-10-04 15:45:01 +0100

    hello world

---
+++ src/main/java/com/JJEP/JJEP/Controllers/IndexController.java
@@ -0,0 +1,12 @@
+package com.JJEP.JJEP.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+}
