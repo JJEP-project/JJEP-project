ce5c7443a4efafe07c3d64df14602d5b2b571a3f
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-06 02:48:33 +0100

    user service created

---
+++ src/main/java/com/JJEP/JJEP/user/UserService.java
@@ -0,0 +1,13 @@
+package com.JJEP.JJEP.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserDAO userDAO;
+
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+}
