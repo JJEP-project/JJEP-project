0702c52b313786ba3b0112ae37b649b8df4d5bad
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-07 02:29:01 +0100

    user dto with password created

---
+++ src/main/java/com/JJEP/JJEP/user/UserWithPasswordDTO.java
@@ -0,0 +1,28 @@
+package com.JJEP.JJEP.user;
+
+import java.time.LocalDateTime;
+
+public class UserWithPasswordDTO extends UserDTO {
+    public String password;
+
+    public UserWithPasswordDTO(Long id, String username, String email, String fullName, LocalDateTime createdAt, LocalDateTime updatedAt, UserRoles role, String password) {
+        super(id, username, email, fullName, createdAt, updatedAt, role);
+        this.password = password;
+    }
+
+    public UserWithPasswordDTO(String username, String email, String fullName, UserRoles role, String password) {
+        super(username, email, fullName, role);
+        this.password = password;
+    }
+
+    public UserWithPasswordDTO() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
