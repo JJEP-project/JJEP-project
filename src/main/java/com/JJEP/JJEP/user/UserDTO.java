7000355a393e4dacbb12b2cecafe30fe09154a8f
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-07 02:28:33 +0100

    parent user DTO created

---
+++ src/main/java/com/JJEP/JJEP/user/UserDTO.java
@@ -0,0 +1,90 @@
+package com.JJEP.JJEP.user;
+
+import java.time.LocalDateTime;
+
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String fullName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private UserRoles role;
+
+    public UserDTO(String username, String email, String fullName, UserRoles role) {
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public UserDTO(Long id, String username, String email, String fullName, LocalDateTime createdAt, LocalDateTime updatedAt, UserRoles role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.role = role;
+    }
+
+    public UserDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UserRoles getRole() {
+        return role;
+    }
+
+    public void setRole(UserRoles role) {
+        this.role = role;
+    }
+}
+
