bd216e78e20cb559d84093362ee6fa8955b9f7e1
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-07 02:49:04 +0100

    getUserById added with mapping

--- src/main/java/com/JJEP/JJEP/user/UserService.java
+++ src/main/java/com/JJEP/JJEP/user/UserService.java
@@ -1,19 +1,29 @@
 package com.JJEP.JJEP.user;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserDAO userDAO;
+    private final ModelMapper modelMapper;
 
-    public UserService(UserDAO userDAO) {
+
+    public UserService(UserDAO userDAO, ModelMapper modelMapper) {
         this.userDAO = userDAO;
+        this.modelMapper = modelMapper;
     }
 
-    public void addUser(User user) {
-        int result = userDAO.insertUser(user);
+    public void addUser(UserWithPasswordDTO user) {
+        User user1 = modelMapper.map(user, User.class);
+        int result = userDAO.insertUser(user1);
         if (result!=1){
             throw new IllegalStateException("User not added");
         }
+    }
+
+    public UserDTO getUserById(int id){
+        User user = userDAO.selectUserById(id).orElseThrow(() -> new IllegalStateException("User not found"));
+        return modelMapper.map(user, UserDTO.class);
     }
 }
