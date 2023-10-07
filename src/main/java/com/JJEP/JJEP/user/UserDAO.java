78384e8f40f88cdf76327cc32f52d4ef6caf27d1
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-07 02:31:00 +0100

    added optional to user dao

--- src/main/java/com/JJEP/JJEP/user/UserDAO.java
+++ src/main/java/com/JJEP/JJEP/user/UserDAO.java
@@ -1,10 +1,12 @@
 package com.JJEP.JJEP.user;
 
+import java.util.Optional;
+
 public interface UserDAO {
     int insertUser(User user);
-    User selectUserById(int id);
-    User selectUserByUsername(String username);
-    User selectUserByEmail(String email);
+    Optional<User> selectUserById(int id);
+    Optional<User> selectUserByUsername(String username);
+    Optional<User> selectUserByEmail(String email);
     int updateUserById(int id, User user);
     int deleteUserById(int id);
 }
