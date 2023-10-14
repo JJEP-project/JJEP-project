4451e78cbbab6cd471bcb300814a7265dabc22a5
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-14 20:51:11 +0100

    crud operations added to service interface

--- src/main/java/com/JJEP/JJEP/user/IUserService.java
+++ src/main/java/com/JJEP/JJEP/user/IUserService.java
@@ -3,6 +3,14 @@ package com.JJEP.JJEP.user;
 import java.util.List;
 
 public interface IUserService {
+    UserResponseDTO findUserById(long id);
+    UserResponseDTO findUserByEmail(String email);
+
     List<UserResponseDTO> findAllUsers();
+
+    void updateUser(long id, UserRegistrationDTO user);
+
     void saveUser(UserRegistrationDTO user);
+
+    void deleteUser(long id);
 }
