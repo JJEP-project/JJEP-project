194dc24d1062f64954d4d19f52ffe9004a4df1ba
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-17 15:24:07 +0100

    interface for application service created

---
+++ src/main/java/com/JJEP/JJEP/application/IApplicationService.java
@@ -0,0 +1,12 @@
+package com.JJEP.JJEP.application;
+
+
+import java.util.List;
+
+public interface IApplicationService {
+        ApplicationResponseDTO findApplicationById(long id);
+        List<ApplicationResponseDTO> findAllApplications();
+        void updateApplication(long id, ApplicationResponseDTO applicationResponseDTO);
+        void saveApplication(ApplicationRequestDTO applicationRequestDTO);
+        void deleteApplication(long id);
+}
