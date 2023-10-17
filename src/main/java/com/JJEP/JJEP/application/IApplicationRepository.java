202e3739345ae3ce3ef0db2dbfbb8c9e3c1e515c
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-17 15:23:12 +0100

    interface for application repository created

---
+++ src/main/java/com/JJEP/JJEP/application/IApplicationRepository.java
@@ -0,0 +1,6 @@
+package com.JJEP.JJEP.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IApplicationRepository extends JpaRepository<Application, Long> {
+}
