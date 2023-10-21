4321fe15e3db3b7fd726cdcf29407f55ba587bc7
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-21 01:29:37 +0100

    application not found exception created

--- src/main/java/com/JJEP/JJEP/application/ApplicationNotFoundException.java
+++ src/main/java/com/JJEP/JJEP/application/ApplicationNotFoundException.java
@@ -1,2 +1,10 @@
-package com.JJEP.JJEP.application;public class ApplicationNotFoundException {
+package com.JJEP.JJEP.application;
+
+public class ApplicationNotFoundException extends RuntimeException {
+    private static final long serialVerisionUID = 3;
+
+    public ApplicationNotFoundException(String message) {
+        super(message);
+
+    }
 }
