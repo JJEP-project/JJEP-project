ef2803894b933ba19eb3f85cf2c8d4c228922c2c
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-21 01:33:42 +0100

    application service added to application controller

--- src/main/java/com/JJEP/JJEP/Controllers/ApplicationController.java
+++ src/main/java/com/JJEP/JJEP/Controllers/ApplicationController.java
@@ -1,11 +1,18 @@
 package com.JJEP.JJEP.Controllers;
 
+import com.JJEP.JJEP.application.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApplicationController {
-    
+    IApplicationService applicationService;
+
+    @Autowired
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
     @GetMapping("/application")
     public String application() {
         return "application";
