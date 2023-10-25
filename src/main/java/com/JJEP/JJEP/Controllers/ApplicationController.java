d0ec6b9100ef32878b5d8f342207a60b0f5c6d34
Yan Chuiko <yc322@student.aru.ac.uk>
2023-10-25 15:59:36 +0100

    Added First Validation

--- src/main/java/com/JJEP/JJEP/Controllers/ApplicationController.java
+++ src/main/java/com/JJEP/JJEP/Controllers/ApplicationController.java
@@ -1,11 +1,16 @@
 package com.JJEP.JJEP.Controllers;
 
 import com.JJEP.JJEP.application.*;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.JJEP.JJEP.application.ApplicationBaseDTO; 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping; 
 
 @Controller
 public class ApplicationController {
@@ -22,4 +27,13 @@ public class ApplicationController {
         return "application";
     }
     
+    @PostMapping("/application-handler")
+    public String applicationHandler(@ModelAttribute("formApplication") @Valid ApplicationBaseDTO formApplication, BindingResult result) {
+        if (result.hasErrors()) {
+            return "application";
+        }
+
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/JJEP/JJEP/application/ApplicationBaseDTO.java
+++ src/main/java/com/JJEP/JJEP/application/ApplicationBaseDTO.java
@@ -1,5 +1,7 @@
 package com.JJEP.JJEP.application;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,17 +13,30 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class ApplicationBaseDTO {
 
-    private Boolean isBloodProtection;
-    private Boolean isGenerationIht;
-    private String notes;
     private float primaryProperty;
+    
     private float ukHolidayHome;
+    
     private float btlProperty;
+    
     private float foreignProperty;
+    
     private boolean foreignWill;
+    
     private float investment;
+    
     private float savingsCash;
+    
     private float total;
+    
     private float personalLifeCover;
+    
     private boolean trust;
+    
+    private Boolean isBloodProtection;
+    
+    private Boolean isGenerationIht;
+    
+    @NotBlank(message = "Notes cannot be blank")
+    private String notes;
 }
