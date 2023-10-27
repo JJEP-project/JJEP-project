d36246d6fd73e92f3f15711a2380d693b20c2087
damirvaliyev <dvaliyev16@gmail.com>
2023-10-27 17:34:39 +0100

    Edited folder structure

---
+++ src/main/java/com/JJEP/JJEP/admin/applications/IndexController.java
@@ -0,0 +1,40 @@
+package com.JJEP.JJEP.admin.applications;
+
+import com.JJEP.JJEP.application.ApplicationResponseDTO;
+import com.JJEP.JJEP.application.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    ApplicationService applicationService;
+
+    @GetMapping("/applicationsadmin")
+    public String adminApplications(Model model) {
+
+
+        List<ApplicationResponseDTO> forms = applicationService.findAllApplications();
+        model.addAttribute("forms", forms);
+
+        return "admin-applications";
+    }
+
+    @GetMapping("/application-details-admin/{id}")
+    public String getApplicationDetails(@PathVariable int id, Model model) {
+
+        ApplicationResponseDTO form = applicationService.findApplicationById(id);
+        model.addAttribute("form", form);
+
+
+        return "application-details";
+
+    }
+
+}
