61e6c20090f8f5403736ed5bb81ee73c45cf2b4b
damirvaliyev <dvaliyev16@gmail.com>
2023-10-27 17:15:11 +0100

    new html templates: user and application details, user edit, user create, new post and get requests

--- src/main/java/com/JJEP/JJEP/admin/AdminController.java
+++ src/main/java/com/JJEP/JJEP/admin/AdminController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import com.JJEP.JJEP.application.Application;
 import com.JJEP.JJEP.application.ApplicationResponseDTO;
 import com.JJEP.JJEP.application.ApplicationService;
+import com.JJEP.JJEP.user.User;
 import com.JJEP.JJEP.user.UserResponseDTO;
 import com.JJEP.JJEP.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
@@ -39,5 +42,44 @@ public class AdminController {
 
         return "admin-applications";
     }
+
+    @GetMapping("/user-details-admin/{id}")
+    public String getUserDetails(@PathVariable int id, Model model) {
+
+        UserResponseDTO user = userService.findUserById(id);
+        model.addAttribute("user", user);
+
+        return "user-details";
+
+    }
+
+    @GetMapping("/application-details-admin/{id}")
+    public String getApplicationDetails(@PathVariable int id, Model model) {
+
+        ApplicationResponseDTO form = applicationService.findApplicationById(id);
+        model.addAttribute("form", form);
+
+
+        return "application-details";
+
+    }
+
+    @GetMapping("/user-create-admin")
+    public String createUser() {
+
+        return "user-create";
+
+    }
+
+    @PostMapping("/user-edit-admin/{id}")
+    public String editUserDetails(@PathVariable int id, Model model) {
+
+        UserResponseDTO user = userService.findUserById(id);
+        model.addAttribute("user", user);
+
+        return "user-edit";
+
+    }
+
 
 }
