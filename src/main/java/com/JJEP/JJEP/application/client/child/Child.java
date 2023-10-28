8bf6cb730d97cc385f73a74cf49de0ab52ed865a
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-28 03:25:24 +0100

    child entity created

--- src/main/java/com/JJEP/JJEP/application/client/child/Child.java
+++ src/main/java/com/JJEP/JJEP/application/client/child/Child.java
@@ -1,5 +1,6 @@
-package com.JJEP.JJEP.application.client.children;
+package com.JJEP.JJEP.application.client.child;
 
+import com.JJEP.JJEP.application.client.Client;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -25,4 +28,12 @@ public class Child {
 
     @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+
+    @ManyToMany
+    @JoinTable(
+            name = "client_children",
+            joinColumns = @JoinColumn(name = "child_id"),
+            inverseJoinColumns = @JoinColumn(name = "client_id")
+    )
+    private Set<Client> clients = new HashSet<>();
 }
