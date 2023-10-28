63d8225b1a91b49a5e3d716636ed425a41cf2ace
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-10-28 03:41:48 +0100

    save all clients added to interface

--- src/main/java/com/JJEP/JJEP/application/client/IClientService.java
+++ src/main/java/com/JJEP/JJEP/application/client/IClientService.java
@@ -1,8 +1,11 @@
 package com.JJEP.JJEP.application.client;
 
+import java.util.List;
+
 public interface IClientService {
         ClientResponseDTO findClientById(long id);
         void updateClient(long id, ClientResponseDTO clientResponseDTO);
-        void saveClient(ClientRequestDTO clientRequestDTO);
+        ClientResponseDTO saveClient(ClientRequestDTO clientRequestDTO);
+        List<ClientResponseDTO> saveAllClients(List<ClientRequestDTO> clientRequestDTOs);
         void deleteClient(long id);
 }
