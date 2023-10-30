42f097fbf4e6caf5e6540271ce3afcbc89d3d7a9
Yan Chuiko <yc322@student.aru.ac.uk>
2023-10-30 14:07:16 +0000

    Complete Validation

--- src/main/java/com/JJEP/JJEP/application/ApplicationBaseDTO.java
+++ src/main/java/com/JJEP/JJEP/application/ApplicationBaseDTO.java
@@ -14,35 +14,35 @@ import lombok.experimental.SuperBuilder;
 public class ApplicationBaseDTO {
 
     @NotNull(message = "Primary Property cannot be blank")
-    @EnsureNumber(message = "Primary Property must be number.")
+    @EnsureNumber(message = "Primary Property must be a valid number.")
     private Float primaryProperty;
     
     @NotNull(message = "UK Holiday Home cannot be blank")
-    @EnsureNumber(message = "UK Holiday Home must be number.")
+    @EnsureNumber(message = "UK Holiday Home must be a valid number.")
     private Float ukHolidayHome;
     
     @NotNull(message = "BTL Property cannot be blank")
-    @EnsureNumber(message = "BTL Property must be number.")
+    @EnsureNumber(message = "BTL Property must be a valid number.")
     private Float btlProperty;
     
     @NotNull(message = "Foreign Property cannot be blank")
-    @EnsureNumber(message = "Foreign Property must be number.")
+    @EnsureNumber(message = "Foreign Property must be a valid number.")
     private Float foreignProperty;
     
     @NotNull(message = "Investment cannot be blank")
-    @EnsureNumber(message = "Investment must be number.")
+    @EnsureNumber(message = "Investment must be a valid number.")
     private Float investment;
     
     @NotNull(message = "Savings & Cash cannot be blank")
-    @EnsureNumber(message = "Savings & Cash must be number.")
+    @EnsureNumber(message = "Savings & Cash must be a valid number.")
     private Float savingsCash;
     
     @NotNull(message = "Total cannot be blank")
-    @EnsureNumber(message = "Total must be number.")
+    @EnsureNumber(message = "Total must be a valid number.")
     private Float total;
     
     @NotNull(message = "Personal Life Cover cannot be blank")
-    @EnsureNumber(message = "Personal Life Cover must be number.")
+    @EnsureNumber(message = "Personal Life Cover must be a valid number.")
     private Float personalLifeCover;
 
     @NotNull(message = "Foreign Will cannot be null")
