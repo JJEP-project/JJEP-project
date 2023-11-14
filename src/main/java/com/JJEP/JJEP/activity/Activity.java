f3973d9a5a8bcb8af03295cf5ddaa24171a914c2
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-11-14 12:10:10 +0000

    activity model created

---
+++ src/main/java/com/JJEP/JJEP/activity/Activity.java
@@ -0,0 +1,35 @@
+package com.JJEP.JJEP.activity;
+
+import com.JJEP.JJEP.user.User;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name="Activity")
+@Table(name="activity")
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "activity_message", nullable = false)
+    private String activityMessage;
+
+    @Column(name = "activity_date", nullable = false, insertable = false)
+    @CreationTimestamp
+    private LocalDateTime activityDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ToString.Exclude private User user;
+}
