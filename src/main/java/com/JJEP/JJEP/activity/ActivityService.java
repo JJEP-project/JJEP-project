32e2c5f5d23a2bbc2d4b5092db2ece34ca51b74e
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-11-19 00:22:35 +0000

    comments added

--- src/main/java/com/JJEP/JJEP/activity/ActivityService.java
+++ src/main/java/com/JJEP/JJEP/activity/ActivityService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.ArrayList;
 
+// service annotation is used to mark the class as a service provider for the spring autowire
 @Service
 public class ActivityService implements IActivityService{
     private final IActivityRepository activityRepository;
@@ -19,17 +20,7 @@ public class ActivityService implements IActivityService{
     @Override
     public List<ActivityResponseDTO> findAllActivities() {
         List<Activity> activities = activityRepository.findAll();
-        List<ActivityResponseDTO> activityResponseDTOS = new ArrayList<>();
-
-        if (activities.isEmpty()) {
-            throw new ActivityNotFoundException("No activities found");
-        }
-
-        for (Activity activity : activities) {
-            activityResponseDTOS.add(modelMapper.map(activity, ActivityResponseDTO.class));
-        }
-
-        return activityResponseDTOS;
+        return getActivityResponseDTOs(activities);
     }
 
     @Override
@@ -41,22 +32,17 @@ public class ActivityService implements IActivityService{
     @Override
     public List<ActivityResponseDTO> findAllActivitiesNewestFirst() {
         List<Activity> activities = activityRepository.findAllByOrderByActivityDateDesc();
-        List<ActivityResponseDTO> activityResponseDTOS = new ArrayList<>();
-
-        if (activities.isEmpty()) {
-            throw new ActivityNotFoundException("No activities found");
-        }
-
-        for (Activity activity : activities) {
-            activityResponseDTOS.add(modelMapper.map(activity, ActivityResponseDTO.class));
-        }
-
-        return activityResponseDTOS;
+        return getActivityResponseDTOs(activities);
     }
 
     @Override
     public List<ActivityResponseDTO> findAllActivitiesOldestFirst() {
         List<Activity> activities = activityRepository.findAllByOrderByActivityDateAsc();
+        return getActivityResponseDTOs(activities);
+    }
+
+    // helper method to get the list of activity response DTOs
+    private List<ActivityResponseDTO> getActivityResponseDTOs(List<Activity> activities) {
         List<ActivityResponseDTO> activityResponseDTOS = new ArrayList<>();
 
         if (activities.isEmpty()) {
--- src/main/java/com/JJEP/JJEP/activity/IActivityRepository.java
+++ src/main/java/com/JJEP/JJEP/activity/IActivityRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// jpa repository is used to reduce boilerplate code
+// as it provides the implementation for the standard CRUD operations
 public interface IActivityRepository extends JpaRepository<Activity, Long> {
     List<Activity> findAllByOrderByActivityDateDesc();
     List<Activity> findAllByOrderByActivityDateAsc();
