b115a96a60abdbcc6c15a09bdd552eca07cb9ee9
Maksym Vorobyov <maxvorobyov01@gmail.com>
2023-11-21 17:25:02 +0000

    analyzer created

---
+++ src/main/java/com/JJEP/JJEP/report/ApplicationAnalyzer.java
@@ -0,0 +1,62 @@
+package com.JJEP.JJEP.report;
+
+import com.JJEP.JJEP.application.ApplicationResponseDTO;
+import com.JJEP.JJEP.application.client.ClientResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ApplicationAnalyzer implements IApplicationAnalyzer{
+    @Override
+    public List<ReportResponseDTO> analyzeApplication(ApplicationResponseDTO applicationResponseDTO) {
+        List<ReportResponseDTO> reportResponseDTOList = new ArrayList<>();
+
+        //analyze joint life cover
+        if (applicationResponseDTO.getPersonalLifeCover() == 0 || applicationResponseDTO.isTrust()) {
+            reportResponseDTOList.add(ReportResponseDTO.builder()
+                    .firstName("Joint")
+                    .lifeCover(jointLifeCoverGood)
+                    .build());
+        }
+        else {
+            reportResponseDTOList.add(ReportResponseDTO.builder()
+                    .firstName("Joint")
+                    .lifeCover(String.format(jointLifeCoverConcern, applicationResponseDTO.getPersonalLifeCover()))
+                    .build());
+        }
+
+        //analyze clients
+        for (ClientResponseDTO client: applicationResponseDTO.getClients()) {
+            ReportResponseDTO reportResponseDTO = ReportResponseDTO.builder()
+                    .firstName(client.getForename())
+                    .build();
+            //analyze personal life cover
+            if (client.getPersonalLifeCover() == 0.0f || client.isTrust()){
+                reportResponseDTO.setLifeCover(String.format(personalLifeCoverGood, client.getForename()));
+            }
+            else {
+                reportResponseDTO.setLifeCover(String.format(personalLifeCoverConcern, client.getForename(), client.getPersonalLifeCover()));
+            }
+
+            //analyze personal pension
+            if (client.getPension() == 0.0f || client.isTrust()){
+                reportResponseDTO.setPersonalPension(String.format(personalPensionGood, client.getForename()));
+            }
+            else {
+                reportResponseDTO.setPersonalPension(String.format(personalPensionConcern, client.getForename(), client.getPension()));
+            }
+
+            //analyze death in service
+            if (client.getDeathInService() == 0.0f || client.isTrust()){
+                reportResponseDTO.setDeathInService(String.format(deathInServiceGood, client.getForename()));
+            }
+            else {
+                reportResponseDTO.setDeathInService(String.format(deathInServiceConcern, client.getForename(), client.getDeathInService()));
+            }
+            reportResponseDTOList.add(reportResponseDTO);
+        }
+        return reportResponseDTOList;
+    }
+}
